9f251e9682dca3304a374d18b0c2aaf3cd45107e
Tamanna0322 <tamanna0322@gmail.com>
2024-05-10 19:19:54 +0600

    added footer section

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,5 +1,6 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar/Navbar";
+import Footer from "../components/Footer/Footer";
 
 
 const Main = () => {
@@ -10,6 +11,8 @@ const Main = () => {
             </div>
             
             <Outlet></Outlet>
+
+            <Footer></Footer>
 
         </div>
     );
