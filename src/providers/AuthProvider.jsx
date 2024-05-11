f6429a02b858add534ed27251658f044650c8510
Tamanna0322 <tamanna0322@gmail.com>
2024-05-11 14:29:12 +0600

    setup firebase and AuthProvider

---
+++ src/providers/AuthProvider.jsx
@@ -0,0 +1,28 @@
+import { createContext, useState } from "react";
+import PropTypes from 'prop-types';
+
+
+export const AuthContext = createContext(null);
+
+const AuthProvider = ({children}) => {
+
+    const [user, setUser] = useState(null);
+
+
+    const userInfo = {
+        user,
+        setUser
+    }
+
+    return (
+        <AuthContext.Provider value={userInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+AuthProvider.propTypes ={
+    children: PropTypes.object
+}
+
+export default AuthProvider;
